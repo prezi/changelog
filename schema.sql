7dfe264e7d01f5c5940d372dbea7b8a1c3a7ece8
Zoltan Nagy <abesto@abesto.net>
2013-06-09 13:34:24 -0700

    Add ; in schema.sql

--- schema.sql
+++ schema.sql
@@ -8,4 +8,4 @@ CREATE TABLE events (
 CREATE INDEX events_criticality_index on events(criticality);
 CREATE INDEX events_unix_timestamp_index ON events(unix_timestamp);
 CREATE INDEX events_category_index ON events(category);
-CREATE UNIQUE INDEX unique_events on events(criticality, unix_timestamp, category, description)
+CREATE UNIQUE INDEX unique_events on events(criticality, unix_timestamp, category, description);
