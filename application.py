11333413623971c5dde16466fe703d7595486db2
Zoltán Nagy <abesto@abesto.net>
2014-03-26 12:51:44 +0100

    Don't crap out if we get unicode in JSON

--- application.py
+++ application.py
@@ -19,15 +19,15 @@ close_db_connection_on_app_teardown(app)
 json_parser = reqparse.RequestParser()
 json_parser.add_argument('criticality', type=int, required=True, location='json')
 json_parser.add_argument('unix_timestamp', type=int, required=True, location='json')
-json_parser.add_argument('category', type=str, required=True, location='json')
-json_parser.add_argument('description', type=str, required=True, location='json')
+json_parser.add_argument('category', type=unicode, required=True, location='json')
+json_parser.add_argument('description', type=unicode, required=True, location='json')
 
 query_parser = reqparse.RequestParser()
-query_parser.add_argument('criticality', type=str)
+query_parser.add_argument('criticality', type=unicode)
 query_parser.add_argument('hours_ago', type=float, required=True)
 query_parser.add_argument('until', type=int)
-query_parser.add_argument('category', type=str)
-query_parser.add_argument('description', type=str)
+query_parser.add_argument('category', type=unicode)
+query_parser.add_argument('description', type=unicode)
 
 
 class EventList(Resource):
